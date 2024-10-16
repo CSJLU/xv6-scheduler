5e356cf3fc4142ed0a3b26957d140cb9563e181f
Evan <ezheng10@binghamton.edu>
2024-10-16 16:24:32 -0400

    set up set_sched syscall

---
+++ base/set_sched.c
@@ -0,0 +1,21 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char * argv[])
+{
+	int i = atoi(argv[1]);
+	
+    if(i<0 || i>1)
+        printf(1,"Available options for fork winner: \n[\n0 = round robin (default),\n1 = stride scheduler\n]");
+    
+    set_sched(i);
+    
+    if(i == 0){
+        printf(1, "Selecting parent first\n");
+    } else if(i == 1){
+        printf(1, "Selecting child first\n");
+    }
+
+    exit(); //return 0;
+}
